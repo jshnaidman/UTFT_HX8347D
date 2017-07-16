c5d57a710f642408737801b8a3219da3a770c83d
jshnaidman <jshnaidman245@gmail.com>
2017-07-16 01:27:28 -0400

    Added project to Git

---
+++ OLD/wave-101_code/DisplayString/DisplayString.ino
@@ -0,0 +1,26 @@
+#include <stdint.h>
+#include <LCD.h>
+#include <SPI.h>
+#define __AVR_ATmega32U4__
+
+void setup()
+{  
+    SPI.setDataMode(SPI_MODE3);
+    SPI.setBitOrder(MSBFIRST);
+    SPI.setClockDivider(SPI_CLOCK_DIV4);
+    SPI.begin();
+    
+    Tft.lcd_init();                                      // init TFT library
+    Tft.lcd_display_string(60, 120, (const uint8_t *)"Hello, world !", FONT_1608, RED);
+    Tft.lcd_display_string(30, 152, (const uint8_t *)"2.8' TFT Touch Shield", FONT_1608, RED);
+}
+
+void loop()
+{
+  
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
+
---
+++ OLD/wave-101_code/DrawGraphic/DrawGraphic.ino
@@ -0,0 +1,31 @@
+#include <stdint.h>
+#include <LCD.h>
+#include <SPI.h>
+
+void setup()
+{
+    SPI.setDataMode(SPI_MODE3);
+    SPI.setBitOrder(MSBFIRST);
+    SPI.setClockDivider(SPI_CLOCK_DIV4);
+    SPI.begin();
+    
+    Tft.lcd_init();                                      // init TFT library
+    
+    Tft.lcd_draw_rect(30, 40, 150, 100, RED);
+    Tft.lcd_draw_circle(120, 160, 50, BLUE);
+    Tft.lcd_draw_line(30, 40, 180, 140, RED);
+    
+    Tft.lcd_draw_line(30, 220, 210, 240, RED);
+    Tft.lcd_draw_line(30, 220, 120, 280, RED);
+    Tft.lcd_draw_line(120, 280, 210, 240, RED);
+}
+
+void loop()
+{
+  
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
+
